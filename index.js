5102b6a64ebbfba1dc1008c8155fa0063f3fa436
Franklin Sahlhoff <franklinsahlhoff@gmail.com>
2021-05-10 11:21:21 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+function appendCat() {
+ let newCats = [...cats, "Broom"];
+ return newCats;
+}
+function prependCat() {
+    let newCats = ["Arnold", ...cats];
+    return newCats;
+   }
+function removeLastCat() {
+   let newCat = cats.slice(0, cats.length - 1);
+   return newCat;
+}   
+function removeFirstCat() {
+    let newCat = cats.slice(1);
+    return newCat;
+}
